64b02bcf77b6c08055ef860f96737d4ea2d91427
Adedamola Ayodeji Sode <71727147+AsimovNo9@users.noreply.github.com>
2026-09-26 01:17:56 +0100

    test: expose scorer diagnostic in service worker console

--- apps/extension/src/background/diagnostic-service-worker.ts
+++ apps/extension/src/background/diagnostic-service-worker.ts
@@ -1,6 +1,10 @@
 import './index.ts';
 import { runPersonalAlgorithmLiveDiagnostic } from './personal-algorithm-diagnostic.ts';
 
+(globalThis as typeof globalThis & {
+  __runPersonalAlgorithmLiveDiagnostic?: typeof runPersonalAlgorithmLiveDiagnostic;
+}).__runPersonalAlgorithmLiveDiagnostic = runPersonalAlgorithmLiveDiagnostic;
+
 chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
   if (message?.type !== 'PERSONAL_ALGORITHM_SCORE_DIAGNOSTIC') return false;
 
